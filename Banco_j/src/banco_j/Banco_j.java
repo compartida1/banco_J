2ccf337e90ad77e37d019b7438a2131e500c5cb9
JavierRiquelme <cuentacompartida496@gmail.com>
2019-02-04 08:44:33 +0100

    Primera modificación Banco_j

--- Banco_j/src/banco_j/Banco_j.java
+++ Banco_j/src/banco_j/Banco_j.java
@@ -5,6 +5,7 @@ public class Banco_j {
 
     public static void main(String[] args) {
         System.out.println("Este el el banco de Javier Riquelme");
+        System.out.println("Esta es la primera mdificación del banco de Javier Riquelme");
     }
     
 }
