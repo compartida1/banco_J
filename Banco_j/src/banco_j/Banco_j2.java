cb25a4eec2ab5e1d0ebbc15a2fede0e0169e5479
JavierRiquelme <cuentacompartida496@gmail.com>
2019-02-05 22:16:21 +0100

    Modificación rama DEVELOP Banco_j2

--- Banco_j/src/banco_j/Banco_j2.java
+++ Banco_j/src/banco_j/Banco_j2.java
@@ -4,5 +4,6 @@ public class Banco_j2 {
     
     public static void main(String[] args) {
         System.out.println("Esta es la modificación de Javier Riquelme.");
+        System.out.println("Modificación de de la rama DEVELOP.");
     }
 }
